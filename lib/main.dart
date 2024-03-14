3155729603a436e16ae53234086bf67e591e4a77
Albin Anthony Hinostroza <79820950+albinrk10@users.noreply.github.com>
2024-03-13 23:33:11 -0500

    Cubit Simple

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
+import 'package:blocs_app/presentation/screens/app.dart';
 import 'package:flutter/material.dart';
-import 'package:blocs_app/config/config.dart';
 
-void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const App());
 
-  @override
-  Widget build(BuildContext context) {
 
-    return MaterialApp.router(
-      title: 'Flutter BLoC',
-      debugShowCheckedModeBanner: false,
-      routerConfig: appRouter,
-      theme: AppTheme( isDarkmode: false ).getTheme(),
-    );
-  }
-}
---
+++ lib/presentation/bloc/01-simple_cubit/username_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:bloc/bloc.dart';
+
+class USernameCubit extends Cubit<String>{
+  USernameCubit() : super('no-username');
+  
+}
