83e87b1bc4e39ba46e72258cb46b8993738edd64
Albin Anthony Hinostroza <79820950+albinrk10@users.noreply.github.com>
2024-03-16 22:26:56 -0500

    Bloc Events - Relacionado al fIltro

--- lib/presentation/bloc/04-guests/guests_event.dart
+++ lib/presentation/bloc/04-guests/guests_event.dart
@@ -6,3 +6,14 @@ sealed class GuestsEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+final class SetAllFilterEvent extends GuestsEvent {}
+
+final class SetInvitedFilterEvent extends GuestsEvent {}
+
+final class SetNoInvitedFilterEvent extends GuestsEvent {}
+
+final class SetCustomFilterEvent extends GuestsEvent {
+  final GuestFilter newFilter;
+  const SetCustomFilterEvent(this.newFilter);
+}
